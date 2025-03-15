e68dba0045a5a63c566612159e0855d724bd8e63
Evgeniy Mozyakov <evgeniy.mozyakov@gmail.com>
2025-03-15 15:32:14 +0300

    Initial commit: базовая версия сайта рецептов

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipe/<int:pk>/', views.recipe_detail, name='recipe_detail'),
-    path('add/', views.add_recipe, name='add_recipe'),
-    path('register/', views.register, name='register'),
+    path('add/', views.add_recipe, name='add_recipe'),  # Маршрут для добавления рецепта
+    path('edit/<int:pk>/', views.edit_recipe, name='edit_recipe'),  # Маршрут для редактирования рецепта
     path('category/<int:category_id>/', views.category_recipes, name='category_recipes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Recipe
-from .forms import RecipeForm
-from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import RegisterForm
-from django.shortcuts import render, get_object_or_404
 from .models import Recipe, Category
+from .forms import RecipeForm, RegisterForm
 from django.core.paginator import Paginator
 
+# Главная страница
 def home(request):
     recipes = Recipe.objects.all().order_by('?')
     paginator = Paginator(recipes, 6)  # 6 рецептов на страницу
@@ -17,10 +14,12 @@ def home(request):
     categories = Category.objects.all()
     return render(request, 'recipes/home.html', {'page_obj': page_obj, 'categories': categories})
 
+# Страница деталей рецепта
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
 
+# Добавление рецепта
 @login_required
 def add_recipe(request):
     if request.method == 'POST':
@@ -29,12 +28,13 @@ def add_recipe(request):
             recipe = form.save(commit=False)
             recipe.author = request.user
             recipe.save()
-            form.save_m2m()
+            form.save_m2m()  # Сохраняем связанные категории
             return redirect('home')
     else:
         form = RecipeForm()
     return render(request, 'recipes/add_recipe.html', {'form': form})
 
+# Регистрация пользователя
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -46,7 +46,21 @@ def register(request):
         form = RegisterForm()
     return render(request, 'recipes/register.html', {'form': form})
 
+# Фильтрация рецептов по категориям
 def category_recipes(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     recipes = Recipe.objects.filter(categories=category)
     return render(request, 'recipes/category_recipes.html', {'category': category, 'recipes': recipes})
+
+# Редактирование рецепта
+@login_required
+def edit_recipe(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk, author=request.user)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_detail', pk=recipe.pk)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipes/add_recipe.html', {'form': form})
